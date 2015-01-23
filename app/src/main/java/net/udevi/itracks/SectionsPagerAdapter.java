d52de8545fb8b57c0c3db2a1e05275c8ea9fa5a5
Monomachus <monomachus85@yahoo.com>
2015-01-23 04:21:33 +0200

    First commit of the code

---
+++ app/src/main/java/net/udevi/itracks/SectionsPagerAdapter.java
@@ -0,0 +1,41 @@
+package net.udevi.itracks;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.Locale;
+
+/**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return PlaceholderFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public int getCount() {
+            // Show 2 total pages.
+            return 2;
+        }
+
+        public Integer getPageTitleResourceName(int position) {
+            switch (position) {
+                case 0:
+                    return R.string.title_tabWeb;
+                case 1:
+                    return R.string.title_tabLocal;
+            }
+            return null;
+        }
+    }
