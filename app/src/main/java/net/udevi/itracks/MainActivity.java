f916a5672adce8de99b3046da5336130f82d6a90
Monomachus <monomachus85@yahoo.com>
2015-01-26 10:14:12 +0200

    Added SearchView to MainActivity

--- app/src/main/java/net/udevi/itracks/MainActivity.java
+++ app/src/main/java/net/udevi/itracks/MainActivity.java
@@ -2,6 +2,9 @@ package net.udevi.itracks;
 
 import java.util.Locale;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -10,6 +13,7 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.SearchView;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -20,6 +24,8 @@ import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity implements ActionBar.TabListener {
+
+    public static final String QUERY_KEY = "query";
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -76,15 +82,58 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
                             .setText(getString(mSectionsPagerAdapter.getPageTitleResourceName(i)).toUpperCase(l))
                             .setTabListener(this));
         }
+
+        Intent intent = getIntent();
+        // Special processing of the incoming intent only occurs if the if the action specified
+        // by the intent is ACTION_SEARCH.
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            handleIntent(intent);
+        }
     }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        handleIntent(intent);
+    }
+
+    /**
+     * Assuming this activity was started with a new intent, process the incoming information and
+     * react accordingly.
+     * @param intent
+     */
+    private void handleIntent(Intent intent) {
+        // SearchManager.QUERY is the key that a SearchManager will use to send a query string
+        // to an Activity.
+        String query = intent.getStringExtra(SearchManager.QUERY);
+
+        // We need to create a bundle containing the query string to send along to the
+        // LoaderManager, which will be handling querying the database and returning results.
+        Bundle bundle = new Bundle();
+        bundle.putString(QUERY_KEY, query);
+
+//            ContactablesLoaderCallbacks loaderCallbacks = new ContactablesLoaderCallbacks(this);
+//
+//            // Start the loader with the new query, and an object that will handle all callbacks.
+//            getLoaderManager().restartLoader(CONTACT_QUERY_LOADER, bundle, loaderCallbacks);
+    }
+
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        // Get the SearchView and set the searchable configuration
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        // Assumes current activity is the searchable activity
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
         return true;
     }
+
+    // <editor-fold desc="tabListener implementation">
 
     @Override
     public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
@@ -101,6 +150,6 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
     public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
     }
 
-
+    // </editor-fold>
 
 }
